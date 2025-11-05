479d05bf04462df6c61a84a9d8df328d2bd50e8c
Md Refat Hossain <refatibnanwar@gmail.com>
2025-11-05 22:13:52 +0800

    Journal Context done for state management

--- context/journalContext.js
+++ context/journalContext.js
@@ -5,6 +5,7 @@ export const JournalContext = createContext();
 export function JournalProvider({ children }) {
   const [entries, setEntries] = useState([]);
 
+  // Create a new Entry
   const createEntry = useCallback((date, content) => {
     const newEntry = {
       id: Date.now().toString(),
@@ -15,4 +16,56 @@ export function JournalProvider({ children }) {
     };
     setEntries((prevEntries) => [newEntry, ...prevEntries]);
   }, []);
+
+  // Update an existing entry
+  const editEntry = useCallback((id, content) => {
+    setEntries((prevEntries) =>
+      prevEntries.map((entry) =>
+        entry.id === id ? { ...entry, content, updatedAt: new Date() } : entry
+      )
+    );
+  }, []);
+
+  // Delete an entry
+  const removeEntry = useCallback((id) => {
+    setEntries((prevEntries) => prevEntries.filter((entry) => entry.id !== id));
+  }, []);
+
+  // Get entry by ID
+  const getEntryById = useCallback(
+    (id) => {
+      return entries.find((entry) => entries.id === id);
+    },
+    [entries]
+  );
+
+  // Get entry by date
+  const getEntryByDate = useCallback(
+    (date) => {
+      const dateStart = new Date(date);
+      dateStart.setHours(0, 0, 0, 0);
+      const dateEnd = new Date(date);
+      dateEnd.setHours(23, 59, 59, 59);
+
+      return entries.find(
+        (entry) => entries.date >= dateStart && entry.date <= dateEnd
+      );
+    },
+    [entries]
+  );
+
+  return (
+    <JournalContext.Provider
+      value={{
+        entries,
+        createEntry,
+        editEntry,
+        removeEntry,
+        getEntryByDate,
+        getEntryById,
+      }}
+    >
+      {children}
+    </JournalContext.Provider>
+  );
 }
